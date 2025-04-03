ce159a958d8c4bf0eb335c6375daa716d30c8296
Haidekimasu <cadevillajefferson@gmail.com>
2025-04-02 21:46:04 -0300

    modified:   APPSODI/APPSODI/views.py

--- APPSODI/APPSODI/views.py
+++ APPSODI/APPSODI/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def mi_vista_principal(request):
-    return render(request, 'pagina.html') # Renderiza tu plantilla HTML
+    return render(request, 'pagina.html')
 
 def enlace1(request):
-    return redirect("https://apps.apple.com/cl/app/sodimac-casa-y-construcci%C3%B3n/id532353705?mt=8") # Puedes cambiar esto por una plantilla o redirección
+    return redirect("https://apps.apple.com/cl/app/sodimac-casa-y-construcci%C3%B3n/id532353705?mt=8") 
 
 def enlace2(request):
     return redirect("https://www.bancofalabella.cl/pre-landing?utm_source=Sodimac&utm_medium=QRregional&utm_content=belloto-qr-unificado&utm_campaign=apertura-sodimac&utm_term=259303483&store_id=90")
